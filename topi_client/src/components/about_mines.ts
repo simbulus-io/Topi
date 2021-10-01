70c0c2350bc58f5f8316a73347ec53ef7a6809db
Sean <sean.kelly@wootmath.com>
2021-10-01 12:30:44 -0600

    for Laura

--- topi_client/src/components/about_mines.ts
+++ topi_client/src/components/about_mines.ts
@@ -1,15 +1,22 @@
 import { Component, Vue } from 'vue-property-decorator';
 
+declare const JitsiMeetExternalAPI: any;
+
 @Component({
-  components: {
-  },
+  components: {},
 })
 export default class AboutMines extends Vue {
-
   public message: string = 'A different message';
 
   public async created() {
-    setTimeout( () => this.message = 'And it changes', 5000);
+    setTimeout(() => (this.message = 'And it changes'), 5000);
+    const domain = 'meet.jit.si';
+    const options = {
+      roomName: 'JitsiMeetAPIExample',
+      width: 700,
+      height: 700,
+      parentNode: document.querySelector('#meet'),
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
   }
-
 }
