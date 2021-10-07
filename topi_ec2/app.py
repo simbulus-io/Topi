441a87d6939c6c07f22f7e3b4cb9a40653402eef
Dillon Anderson <dillonanderson2023@gmail.com>
2021-10-07 12:53:07 -0600

    Subprocess exited with error 1 due to not having a public subnet when I have 2

--- topi_ec2/app.py
+++ topi_ec2/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os
-
+from topi_ec2.topi_ec2_stack import TopiEc2Stack
 from aws_cdk import core as cdk
 
 # For consistency with TypeScript code, `cdk` is the preferred import name for
@@ -13,22 +13,8 @@ from topi_ec2.topi_ec2_stack import TopiEc2Stack
 
 
 app = core.App()
-TopiEc2Stack(app, "TopiEc2Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+env = core.Environment(account="525256556709",region="us-east-2")
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
 
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
+TopiEc2Stack(app, "ec2-instance",env=env)
 app.synth()
--- topi_ec2/topi_ec2/topi_ec2_stack.py
+++ topi_ec2/topi_ec2/topi_ec2_stack.py
@@ -1,10 +1,17 @@
-from aws_cdk import core as cdk
+from aws_cdk import(
+ core as cdk,
+ aws_ec2 as ec2,
+)
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+
+vpcID = "vpc-01cb12e35d9d59f47"
+instanceName = "Topi_Server"
+instanceType = "t2.micro"
+amiName = "ami-00399ec92321828f5"
 
 
 class TopiEc2Stack(cdk.Stack):
@@ -12,4 +19,31 @@ class TopiEc2Stack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc.from_lookup(
+            self,
+            "vpc",
+            vpc_id=vpcID
+        )
+
+        sec_group = ec2.SecurityGroup(
+            self,
+            "sec-group-allow-ssh",
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+
+        sec_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4('10.0.0.0/16'),
+            description="Allow SSH connection", 
+            connection=ec2.Port.tcp(22)
+        )
+
+        ec2_instance = ec2.Instance(
+            self,
+            "ec2-instance",
+            instance_name=instanceName,
+            instance_type=ec2.InstanceType(instanceType),
+            machine_image=ec2.MachineImage().lookup(name=amiName),
+            vpc=vpc,
+            security_group=sec_group,
+        )
