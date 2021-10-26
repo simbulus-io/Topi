0634cfa63e80945873aa33ff63d72596cc346180
Kenny <kennybf007@gmail.com>
2021-10-26 12:48:03 -0600

    Delete route working

--- topi_server/src/helpers/user_helper.ts
+++ topi_server/src/helpers/user_helper.ts
@@ -7,9 +7,11 @@ import { MongoDBs } from '../helpers/mongo_helper';
 // login 
 const login = (req: Request, res: Response, next: NextFunction) => {
     
+    // connect to mongo
     const mongo: MongoDBs = req.app.get('mongo');
     const db = mongo.topi_db;
 
+    // set email & pw, then find corresponding pw & match
     let { email, password } = req.body;
     db.collection('users')
     .findOne({ email })
@@ -52,9 +54,12 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 
 // returns all users info - for testing mainly
 const getInfo = async (req: Request, res: Response, next: NextFunction) => {
+
+    // connect to mongo
     const mongo: MongoDBs = req.app.get('mongo');
     const db = mongo.topi_db;
 
+    // return all users in db
     db.collection('users')
     .find()
     .toArray(function (err, users) {
@@ -65,11 +70,14 @@ const getInfo = async (req: Request, res: Response, next: NextFunction) => {
 
 // need to delete user
 const deleteUser = (req: Request, res: Response) => {
+
+    // connect to mongo
     const mongo: MongoDBs = req.app.get('mongo');
     const db = mongo.topi_db;
 
+    // find user off of _id & delete
     db.collection('users')
-    .deleteOne({ _id: req.body.id })
+    .deleteOne({ "id" : req.body.id })
     .then(user => {
         return res.status(200).json({ user })
     })
