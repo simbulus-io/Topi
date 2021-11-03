9048e2b56c86d051cdcd41fba22cb36c09013c93
Kenny <kennybf007@gmail.com>
2021-11-03 17:24:59 -0600

    Testing store.ts

--- topi_client/src/store/store.ts
+++ topi_client/src/store/store.ts
@@ -5,11 +5,32 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: String,
+    events: Array
   },
+
+  // Update state 
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    }
   },
+
+  // async funcs.
   actions: {
   },
+
+  getters: {
+    getUser(state) {
+      return state.user
+    },
+
+    getEvents(state) {
+      return state.events
+    }
+  },
+
   modules: {
   },
+
 });
