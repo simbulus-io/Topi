80c4e29fc8376a91d4e2ebe35f5d38739d78de8f
ynnek <kennybf007@gmail.com>
2021-11-18 19:08:15 -0700

    done w/ forgot pw page

--- topi_client/src/components/views/auth/forgot.vue
+++ topi_client/src/components/views/auth/forgot.vue
@@ -20,7 +20,20 @@
           <button class='theButton' @click='fakeMsg'>Reset password!</button>
         </div>
       </div>
+      <div class="final">
+        <h1 class="titleTwo" >How to protect your account?</h1>
+        <p><i>Some relevant links that show you how to properly keep your account secured...</i></p>
+        <div id="options">
+            <ul id="ul-links">            
+                <li><a href='https://www.nativeintelligence.com/resources/cyber-security-links/'>Native Intelligence</a></li>
+                <li><a href='https://www.upguard.com/blog/the-password-security-checklist'>How to create a secure password?</a></li>
+                <li><a href='https://www.sagaeducation.org/privacy-policy'>Saga Education Privacy Policy</a></li>
+                <li><a href='https://www.mines.edu/campus-safety/'>CSM Security</a></li>
+            </ul>
+        </div> 
+      </div>
     </div>
+    
 </template>
 
 <script lang='ts'>
@@ -39,11 +52,19 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+.final {
+  padding: 40px;
+}
+.titleTwo {
+  font-size:20px;
+  /* padding:15px 0px 0px 0px; */
+  /* margin:20px; */
+  /* background-color: lightgrey; */
+}
 .title {
   padding: 0px;
   margin: 0px;
 }
-
 .mainDiv{
   border: 10px groove #0460AD;
   background-color:lightgrey;
@@ -72,5 +93,38 @@ export default Vue.extend({
 }
 .theButton:hover {
   cursor:pointer;
+}
+/* Link styling */
+li {
+    text-align: center;
+    border-bottom: 1px solid black;
+}
+li a {
+    display: inline-flex;
+    color: black;
+    padding: 8px 8px;
+    text-decoration: none;
+}
+li a:hover {
+    background-color: #111;
+    color: white;
+}
+/* Relevant Links Div */
+#options {
+    border: 5px groove #0460AD;
+    margin: 15px auto;
+    width: 300px;
+    text-align: center;
+    background-color: lightgrey;  
+}
+
+/* Relevant Links List */
+#ul-links{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 300px;
+    background-color: lightgrey;
+    overflow: hidden;
 }
 </style>
