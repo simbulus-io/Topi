59f679dc5d300e18d5071bed9d461eb61082ce03
ynnek <kennybf007@gmail.com>
2021-11-22 20:11:22 -0700

    Home pge

--- topi_client/src/components/Home.vue
+++ topi_client/src/components/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  
+  <div>
+    <div class="welcome-topi">
+      <h1>Welcome to the TOPI project</h1>
+    </div>
+    <div class="creators">
+      <div>
+        <img class="pic" src='../assets/pfp.png'>
+        <h3>KF</h3>
+      </div>
+      <div>
+        <img class="pic" src='../assets/pfp.png'>
+        <h3>DA</h3>
+      </div>
+      <div>
+        <img class="pic" src='../assets/pfp.png'>
+        <h3>LB</h3>
+      </div>
+      <div>
+        <img class="pic" src='../assets/pfp.png'>
+        <h3>AJ</h3>
+      </div>
+      
+      
+    </div>
+    
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,7 +34,12 @@ export default class Home extends Vue {}
 </script>
 
 <style scoped>
-.page {
-  height:100vh;
+.creators {
+  display:inline-flex;
 }
+.pic {
+  padding: 9px;
+}
+
+
 </style>
